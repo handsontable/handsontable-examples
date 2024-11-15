5e61b74a951b66501f4ea2afffbd6fb758d50fd0
adrianspdev <adrian.dusinkiewicz@soppo.team>
2024-11-15 14:28:57 +0100

    Update iframe config

--- us-summit-demo/src/App.tsx
+++ us-summit-demo/src/App.tsx
@@ -43,7 +43,7 @@ function App() {
       <Header />
       <ThemeBar theme={theme} setTheme={setTheme} />
       <Table key={theme} themeName={theme} />
-      <iframe className="demo-iframe" src="https://stackblitz.com/github/handsontable/handsontable-examples/tree/master/us-summit-demo?view=editor"/>
+      <iframe className="demo-iframe" src="https://stackblitz.com/github/handsontable/handsontable-examples/tree/master/us-summit-demo?view=edito&file=src%2FApp.tsx"/>
     </div>
   );
 }
